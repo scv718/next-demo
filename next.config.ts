d95fe0c5df0c5a9b308f4db3c1189500ddbc3fda
scv718 <scv718@nbware.co.kr>
2025-07-25 13:30:00 +0900

    로그아웃 기능 추가 및 주소 수정

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  experimental: {
-    webpackBuildWorker: false, // 기존 설정 있을 경우 병합
-    serverActions: false, // 필요 없다면 false
-    // 👇 여기서 watchOptions 사용
-    webpackDevMiddleware: {
-      watchOptions: {
-        poll: 1000,
-        aggregateTimeout: 300,
-      },
-    },
-  },
+  /* config options here */
+  output: "standalone", //운영환경시 사용
 };
 
 export default nextConfig;
