a0aa756da672cf8570f15563bc857fabba04257e
scv718 <scv718@nbware.co.kr>
2025-07-25 11:14:52 +0900

    자동 재시작 설정

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone", //운영환경시 사용
+  output: "standalone",
+  experimental: {
+    webpackBuildWorker: false, // 기존 설정 있을 경우 병합
+    serverActions: false, // 필요 없다면 false
+    // 👇 여기서 watchOptions 사용
+    webpackDevMiddleware: {
+      watchOptions: {
+        poll: 1000,
+        aggregateTimeout: 300,
+      },
+    },
+  },
 };
 
 export default nextConfig;
