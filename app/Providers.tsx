af1603e14aac893c4668a89f27c8884e24b5c0b6
scv718 <scv718@nbware.co.kr>
2025-08-05 16:50:58 +0900

    로그인 세션 관련 수정

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -2,6 +2,14 @@
 
 import { ThemeProvider } from 'next-themes';
 
+import { SessionProvider } from 'next-auth/react';
+
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute='class'>{children}</ThemeProvider>;
+  return (
+    <SessionProvider>
+      <ThemeProvider attribute='data-theme' defaultTheme='light'>
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
